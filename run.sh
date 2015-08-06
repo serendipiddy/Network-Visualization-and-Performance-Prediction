0ec321f6c1ddb35826f787577b725529493061e9
iddy <lemon.and.lime@hotmail.com>
2015-08-06 20:34:01 +1200

    wrote run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+SRC="~/vis"
+DST="~/ryu/ryu/app/"
+RYU="~/ryu"
 
+cp $SRC/perfte.py $DST/perfte.py
+cp $SRC/simple_switch_13.py $DST/simple_switch_13.py
+cp $SRC/gui_vis $DST/gui_vis
+
+PYTHONPATH=$RYU $RYU/ryu/bin/ryu-manager --observe-links $RYU/ryu/app/gui_vis/gui_vis.py
