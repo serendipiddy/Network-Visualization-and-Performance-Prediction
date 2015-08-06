bfa7434ba592dc3bef0f4675f7bfb176771e5de0
iddy <lemon.and.lime@hotmail.com>
2015-08-06 20:37:47 +1200

    wrote run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-SRC="home/mininet/vis"
-DST="home/mininet/ryu/ryu/app/"
-RYU="home/mininet/ryu"
+SRC="/home/mininet/vis"
+DST="/home/mininet/ryu/ryu/app/"
+RYU="/home/mininet/ryu"
 
 cp $SRC/perfte.py $DST/perfte.py
 cp $SRC/simple_switch_13.py $DST/simple_switch_13.py
