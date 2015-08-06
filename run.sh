39054967bbfd043d83b94a237f8d69725d8af056
iddy <lemon.and.lime@hotmail.com>
2015-08-06 21:05:56 +1200

    verbose

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ cp $SRC/perfte.py $DST/perfte.py
 cp $SRC/simple_switch_13.py $DST/simple_switch_13.py
 cp -r $SRC/gui_vis $DST
 
-PYTHONPATH=$RYU $RYU/bin/ryu-manager --observe-links $RYU/ryu/app/gui_vis/gui_vis.py
+PYTHONPATH=$RYU $RYU/bin/ryu-manager --observe-links $RYU/ryu/app/gui_vis/gui_vis.py --verbose
