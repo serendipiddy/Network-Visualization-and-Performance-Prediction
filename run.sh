91f8413ab50d86d687d0187db0ded0232b284022
iddy <lemon.and.lime@hotmail.com>
2015-08-06 20:39:35 +1200

    wrote run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ RYU="/home/mininet/ryu"
 
 cp $SRC/perfte.py $DST/perfte.py
 cp $SRC/simple_switch_13.py $DST/simple_switch_13.py
-cp $SRC/gui_vis $DST/gui_vis
+cp -r $SRC/gui_vis $DST/gui_vis
 
-PYTHONPATH=$RYU $RYU/ryu/bin/ryu-manager --observe-links $RYU/ryu/app/gui_vis/gui_vis.py
+PYTHONPATH=$RYU $RYU/bin/ryu-manager --observe-links $RYU/ryu/app/gui_vis/gui_vis.py
