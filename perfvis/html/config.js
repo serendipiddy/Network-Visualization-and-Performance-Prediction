1744e70a2ed92431122c0a3a5472015d1cc9ecc4
iddy-489vm <iddyiddyiddy@gmail.com>
2015-08-25 19:38:38 +1200

    added config and mm1k file

---
+++ perfvis/html/config.js
@@ -0,0 +1,58 @@
+var config = {
+  'switch_default': 'openvswitch_vm',
+  'model_default':  'mm1_basic',
+  'get_model': {},
+  'switch_configs': {
+    /* The 'brand'/name of the switch determines mean service rate */
+    /* 'pica8' 'openWRT' 'facebook' etc */
+    'openvswitch_vm': {
+                        'service_rate': 56625,
+                        'service_variance':0,
+                      },
+    'pica8':  {
+                'service_rate': 9001,
+                'service_variance':0,
+              },
+  },
+  'queueing_models': {
+    /* each model is stored in 'model/name.js' */
+    'mm1_basic':  { 
+                    'description': 'Simple M/M/1 model, runs per node',
+                    'model_in': [ 'service_rate', 'arrival_rate' ],
+                    'model_out': [ 'load', 'length', 'sojourn' ],
+                  },
+    'mm1k_basic':  { 
+                    'description': 'Simple M/M/1/K model, runs per node',
+                    'model_in': [ 'service_rate', 'arrival_rate', 'queue_capacity' ],
+                    'model_out': [ 'load', 'length', 'sojourn' ],
+                  },
+  },
+}
+
+var output = function(model_name, input) {
+    var model_config = config.queueing_models[model_name];
+    var model = config.get_model[model_name];
+    
+    var model_in = {};
+    /* Check input data */
+    for (var i = 0; i < model_config.model_in.length; i++) {
+        val = model_config.model_in[i];
+        if (!val in input) {
+          console.log('Model required input \''+val+'\' not found');
+          return;
+        }
+        /* check that each val is valid */
+        model_in[val] = input[val];
+    }
+    
+    model.set_input(model_in);
+    
+    var results = {};
+    /* Construct results from configuration */
+    for (var i = 0; i < model_config.model_out.length; i++) {
+        fn = model_config.model_out[i];
+        results[fn] = model[fn]();
+    }
+    
+    return results;
+}
---
+++ perfvis/html/mm1k.js
@@ -0,0 +1,51 @@
+var mm1k_basic = {
+  lam:0,
+  mu:0,
+  rho:0,
+  k:0,
+  len:-1,
+  set_input: function(input) {
+    this.lam = input.arrival_rate;
+    this.mu  = input.service_rate;
+    this.rho = this.lam/this.mu;
+    if (input.queue_capacity < 0) {
+      console.log("Error: queue capacity cannot be less than 0");
+      return;
+    }
+    this.k   = input.queue_capacity;
+  },
+  load: function() {
+    return this.rho;
+  },
+  sojourn: function() {
+    if (this.len == -1) { // avoid a double calculation
+      return this.length/this.lam_eff();
+    }
+    return this.len/this.lam_eff();
+  },
+  length: function() {
+    if (this.len == -1) { // avoid a double calculation
+      var a = this.rho * (1 - (this.k+1)*Math.pow(this.rho,this.k) + this.k*Math.pow(this.rho, this.k+1));
+      var b = (1 - this.rho)*(1 - Math.pow(this.rho,this.k+1));
+      this.len = a/b;
+    }
+    return this.len;
+  },
+  pi0: function() {
+    if (this.lam == this.mu) {
+      return 1 / (this.k + 1);
+    }
+    return (1-this.rho) / (1-Math.pow(this.rho,this.k+1))
+  },
+  pij: function(j) {
+    if (this.lam == this.mu) {
+      return 1 / (this.k + 1);
+    }
+    return Math.pow(this.rho,j) * this.pi0();
+  },
+  lam_eff: function() {
+    return this.lam * (1 - this.pij(this.k));
+  }
+}
+
+config.get_model['mm1k_basic'] = mm1k_basic;
